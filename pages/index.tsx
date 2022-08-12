5f38bb09d83b1dc9f828a7cf956b05e61ff1c5cc
Suzu Tomo <kamekyame@outlook.com>
2022-08-12 20:13:42 +0900

    fix: restore index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,2 @@
-import type { GetServerSideProps } from "next";
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    redirect: {
-      destination: "https://kamekyame.com",
-      permanent: true,
-    },
-  };
-};
-
 const Home = () => <></>;
 export default Home;
